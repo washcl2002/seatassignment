32e77f55b9d802d6f570ffe55532264c33ea55ab
Jonathan <jonppinlac@gmail.com>
2015-10-17 15:35:36 -0400

    id

--- seat.js
+++ seat.js
@@ -2,14 +2,18 @@ window.onload = function() {
 
 for ( var i = 0; i<24; i++) {
     var seat = document.body.appendChild(document.createElement('div'));
-    seat.className = 'available'
-    seat.innerHTML = "<img src='http://www.cliparthut.com/clip-arts/595/movie-theater-seats-clip-art-595331.png'/>"
     seat.addEventListener("click", gotClicked);
+    seat.className = 'available';
+    seat.id = "seat" + (i+1);
+    seat.innerHTML = "<img src='http://www.cliparthut.com/clip-arts/595/movie-theater-seats-clip-art-595331.png'/>"
 }
 
 function gotClicked() {
     if (seat.className === 'available')
         seat.className = 'unavailable';
+    else
+        seat.className = 'available';
+    console.log('got clicked');
 
 }
 
